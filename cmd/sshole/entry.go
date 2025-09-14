18799533d36c112e708aa7818ec3112d06da6042
117503445 <t117503445@gmail.com>
2025-09-14 14:20:26 +0000

    wip
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- cmd/sshole/entry.go
+++ cmd/sshole/entry.go
@@ -4,9 +4,25 @@ import (
 	"context"
 
 	"github.com/rs/zerolog/log"
+
+	chclient "github.com/jpillora/chisel/client"
 )
 
 func cmdEntry(ctx context.Context) {
 	logger := log.Ctx(ctx)
 	logger.Info().Msg("Starting entry")
+
+	c, err := chclient.NewClient(&chclient.Config{
+		Server:  "localhost:9000",
+		Remotes: []string{"8081:localhost:8000"},
+	})
+	if err != nil {
+		logger.Panic().Err(err).Msg("Failed to create chisel client")
+	}
+	if err := c.Start(ctx); err != nil {
+		logger.Panic().Err(err).Msg("Failed to start chisel client")
+	}
+	if err := c.Wait(); err != nil {
+		logger.Panic().Err(err).Msg("Failed to wait chisel client")
+	}
 }
--- go.mod
+++ go.mod
@@ -17,6 +17,7 @@ require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/jpillora/ansi v1.0.3 // indirect
+	github.com/jpillora/backoff v1.0.0 // indirect
 	github.com/jpillora/requestlog v1.0.0 // indirect
 	github.com/jpillora/sizestr v1.0.0 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
