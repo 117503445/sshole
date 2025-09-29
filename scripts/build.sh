5707b866da0e25488572dc1577c4e538ffc8bcc0
117503445 <t117503445@gmail.com>
2025-09-29 07:56:35 +0000

    wip
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 garble build -ldflags="-s -w" -o ./bin/sshole_linux_amd64 ./cmd/sshole
-CGO_ENABLED=0 GOOS=linux GOARCH=arm64 garble build -ldflags="-s -w" -o ./bin/sshole_linux_arm64 ./cmd/sshole
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 garble build -ldflags="-s -w" -o ./bin/sshole_darwin_amd64 ./cmd/sshole
-CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 garble build -ldflags="-s -w" -o ./bin/sshole_darwin_arm64 ./cmd/sshole
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 garble build -ldflags="-s -w" -o ./bin/sshole_windows_amd64.exe ./cmd/sshole
-CGO_ENABLED=0 GOOS=windows GOARCH=arm64 garble build -ldflags="-s -w" -o ./bin/sshole_windows_arm64.exe ./cmd/sshole
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 garble -tiny build -ldflags="-s -w" -o ./bin/sshole_linux_amd64 ./cmd/sshole
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 garble -tiny build -ldflags="-s -w" -o ./bin/sshole_linux_arm64 ./cmd/sshole
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 garble -tiny build -ldflags="-s -w" -o ./bin/sshole_darwin_amd64 ./cmd/sshole
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 garble -tiny build -ldflags="-s -w" -o ./bin/sshole_darwin_arm64 ./cmd/sshole
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 garble -tiny build -ldflags="-s -w" -o ./bin/sshole_windows_amd64.exe ./cmd/sshole
+CGO_ENABLED=0 GOOS=windows GOARCH=arm64 garble -tiny build -ldflags="-s -w" -o ./bin/sshole_windows_arm64.exe ./cmd/sshole
